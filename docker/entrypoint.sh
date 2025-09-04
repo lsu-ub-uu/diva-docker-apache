8df480a35015e0bb92142dda0bc5a285a84f3c48
perebartrolisimo <pere.bartroli@ub.uu.se>
2025-09-04 12:03:09 +0000

    dynamic vhost for divaclient

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 : "${FITNESSE_CONTEXT_ROOT_ARG:=someContextRoot}"
+: "${DIVA_CLIENT_CONTEXT_ROOT_ARG:=someContextRoot}"
 
 sed -i "s|{FITNESSE_CONTEXT_ROOT}|${FITNESSE_CONTEXT_ROOT_ARG}|g" /usr/local/apache2/conf/extra/cora-vhosts.conf
+sed -i "s|{DIVA_CLIENT_CONTEXT_ROOT}|${DIVA_CLIENT_CONTEXT_ROOT_ARG}|g" /usr/local/apache2/conf/extra/cora-vhosts.conf
 
 supervisord -c /etc/supervisor/conf.d/supervisord.conf
